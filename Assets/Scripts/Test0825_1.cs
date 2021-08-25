6bcbca08b09f8da8d232fe1fbe9d4b3788a80ddf
ShuxinWen <1390306403@qq.com>
2021-08-25 23:20:08 +0800

    ButtonDemo
    Unity不规则按钮Demo

---
+++ Assets/Scripts/Test0825_1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test0825_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {    
+        //按钮注册点击事件
+        this.gameObject.GetComponent<Button>().onClick.AddListener(() => 
+        {
+            Debug.Log("点击了Unity图标按钮！");
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Test0825_2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test0825_2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //alphaHitTestMinimumThreshold 范围值：0 ~ 1 。
+        this.gameObject.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+        //按钮注册点击事件
+        this.gameObject.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Debug.Log("点击了Unity图标按钮！");
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
